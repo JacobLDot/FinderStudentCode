626115364d1f7e04bbf64ffb225b07a3872b4b11
jacobldot <jacob.lowe@menloschool.org>
2025-11-01 17:30:07 -0700

    Finder: - Work Session 1

--- src/Finder.java
+++ src/Finder.java
@@ -6,17 +6,47 @@ import java.io.IOException;
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: [Jacob Lowe]
  **/
 
 public class Finder {
 
+    // Value if key is not found in the table
     private static final String INVALID = "INVALID KEY";
 
-    public Finder() {}
+    // Number of KeyValues in the table
+    private static final int TABLE_SIZE = 1000000;
 
+    // Table of KeyValues
+    private KeyValue[] table;
+
+    /**
+     * Constructor:
+     * Initializes the table of KeyValues
+     */
+    public Finder() {
+        table = new KeyValue[TABLE_SIZE];
+    }
+
+    /**
+     * Method:
+     * Reads each line from the CSV and extracts the key and value from each column
+     */
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
+        String line;
+
+        // Go through every line from the CSV and split each line by commas
+        // to separate columns, then extract the key and value from the column
+        while ((line = br.readLine()) != null) {
+            String[] columns = line.split(",", -1);
+            String key = columns[keyCol];
+            String value = columns[valCol];
+
+            // Add the key and value to the table
+            table[Integer.parseInt(value)] = new KeyValue(key, value);
+        }
+
+        // Close resource
         br.close();
     }
 
---
+++ src/KeyValue.java
@@ -0,0 +1,12 @@
+/**
+ * Class:
+ * Store key-value pairs both as strings
+ */
+public class KeyValue {
+    String key;
+    String value;
+    KeyValue (String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
