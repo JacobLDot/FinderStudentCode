621cb30211fb8fa8fe6c55fafe98d50467569e5d
jacobldot <jacob.lowe@menloschool.org>
2025-11-03 12:29:07 -0800

    Finder: - Work Session 2

--- src/Finder.java
+++ src/Finder.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Finder
  * A puzzle written by Zach Blick
@@ -18,14 +20,30 @@ public class Finder {
     private static final int TABLE_SIZE = 1000000;
 
     // Table of KeyValues
-    private KeyValue[] table;
+    private ArrayList<KeyValue>[] table;
+
+    /**
+     * Helper Class:
+     * Store key-value pairs both as strings
+     */
+    private static class KeyValue {
+        public String key;
+        public String value;
+        public KeyValue (String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     /**
      * Constructor:
      * Initializes the table of KeyValues
      */
     public Finder() {
-        table = new KeyValue[TABLE_SIZE];
+        table = new ArrayList[TABLE_SIZE];
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            table[i] = new ArrayList<>();
+        }
     }
 
     /**
@@ -42,16 +60,46 @@ public class Finder {
             String key = columns[keyCol];
             String value = columns[valCol];
 
+            long keyHash = hash(key);
+            int index = (int)(keyHash % TABLE_SIZE);
             // Add the key and value to the table
-            table[Integer.parseInt(value)] = new KeyValue(key, value);
+
         }
 
         // Close resource
         br.close();
     }
 
+    /**
+     * Method:
+     * Computes a hash value from a string
+     */
+    public static long hash(String key) {
+        long hash = 0;
+        int RADIX = 256;
+        long p = 54321102419L;
+        for (int i = 0; i < key.length(); i++) {
+            hash = (hash * RADIX + key.charAt(i)) % p;
+        }
+        return hash;
+    }
+
+    /**
+     * Method:
+     * Returns value associated with a key
+     */
     public String query(String key){
-        // TODO: Complete the query() function!
+        long keyHash = hash(key);
+        int index = (int)(keyHash % TABLE_SIZE);
+
+        // Search through the list for the exact key
+        for (KeyValue keyValue : table[index]) {
+            if (keyValue.key.equals(key)) {
+                return keyValue.value;
+            }
+        }
+
+        // If not found, return INVALID
         return INVALID;
     }
 }
--- src/KeyValue.java
+++ src/KeyValue.java
@@ -1,12 +1,1 @@
-/**
- * Class:
- * Store key-value pairs both as strings
- */
-public class KeyValue {
-    String key;
-    String value;
-    KeyValue (String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-}
+
